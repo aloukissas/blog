8e5630408e7f84ce7d9b850dabf5f6e7153015c3
Alex Loukissas <aloukissas@gmail.com>
2023-01-02 20:10:32 -0800

    Use astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
 });
