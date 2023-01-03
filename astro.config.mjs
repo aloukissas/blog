ae9145933f229e16a87a4be5a5782174080edf0a
Alex Loukissas <aloukissas@gmail.com>
2023-01-02 20:27:49 -0800

    Update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://alexloukissas.com",
   integrations: [
     mdx(),
     sitemap(),
